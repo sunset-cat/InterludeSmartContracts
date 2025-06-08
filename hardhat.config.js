d800125cc305376b96d35d65ab2216ce7866b7cc
sunset_cat <39526963+sunset-cat@users.noreply.github.com>
2025-06-08 22:59:19 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,11 +8,6 @@ module.exports = {
       url: "https://polygon-mainnet.infura.io/v3/4fecf179182b4b73a8f558433a4f6c8a",
       accounts: [`406f3b565cb6d0ccb0e5ad78d0d907c5622637e3cf796e799ddbf1a05f76b06e`]
     },
-    localhost: {
-      url: "http://127.0.0.1:8545",
-      chainId: 31337,
-      accounts: [`0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80`]
-    },
     cronos: {
       url: "https://cronos-evm-rpc.publicnode.com",
       chainId: 25,
